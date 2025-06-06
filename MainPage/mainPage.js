5d69d0b873a7022b0e3be42eee1ccc3bdd7fc470
IuryRezende <iuryrezende5098@gmail.com>
2025-06-06 04:20:28 -0300

    Finalizado

--- MainPage/mainPage.js
+++ MainPage/mainPage.js
@@ -1,5 +1,10 @@
 const btnCleanEarth = document.getElementById("playNow1");
+const btnGaiaPath = document.getElementById("playNow2");
 
 btnCleanEarth.addEventListener("click", () => {
     window.location.href = "RecicleSystem/cleanEarth.html";
+})
+
+btnGaiaPath.addEventListener("click", () => {
+    window.open("https://scratch.mit.edu/projects/1184276804/", "_blank");
 })
